d8a99002063020ab155d8812de41a18e371426a9
aashir02 <120391556+aashir02@users.noreply.github.com>
2025-07-02 13:14:43 +0530

    Add files via upload

---
+++ Hello1.java
@@ -0,0 +1,12 @@
+public class Hello1{
+    public static void main(String[] args) {
+        for(int i = 1;i<=3;i++){
+            char j = 'b';
+            while(j<='h'){
+                System.out.print(j);
+                j++;
+            }
+            System.out.println();
+        }
+    }
+}
