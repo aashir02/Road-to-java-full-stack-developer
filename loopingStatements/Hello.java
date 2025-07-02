f5f0fcde0938ba59f0f8b3fe04da486a3d9542e4
aashir02 <120391556+aashir02@users.noreply.github.com>
2025-07-02 13:08:31 +0530

    Add files via upload

---
+++ loopingStatements/Hello.java
@@ -0,0 +1,10 @@
+public class Hello{
+    public static void main(String[] args) {
+        for(int i = 1;i<=3;i++){
+            for(char j='b';j<='h';j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
